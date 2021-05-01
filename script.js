76cdfa02f93f3c1727850a483b241a06bc112662
kajal-git-hub <75786022+kajal-git-hub@users.noreply.github.com>
2021-05-01 21:41:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const quizDB = [
+    {
+        question: "Q1: What is the full form of HTML?",
+        a: "Hello To My Land",
+        b: "Hey Text Markup Language",
+        c: "Hypertext Markup Language",
+        d: "Hypertext Markup Language",
+        ans:"ans4"
+    },
+    {
+        question: "Q2: What is the full form of CSS?",
+        a: "Cascading Style Sheets",
+        b: "Cascading Style Sheep",
+        C: "Cascading Super Sheets",
+        d: "Cascading Super Sheets",
+        ans: "ans1" 
+    },
+
+    {
+        question: " Q3: What is the full form of HTTP?",
+        a: "Hypertext Tra",
+        b: "Hypertext Test Protocol",
+        c: "Hey Transfer Protocol",
+        d: "Hypertext Transfer Protocol",
+        ans: "ans4"
+    },{
+        question: "Q4: What is the full form of JS",
+        a:"JavaScript",
+        b:"JavaSuper",
+        c:"JustScript",
+        d:"JordenShoes",
+        ans:"ans1"
+    }
+];  
